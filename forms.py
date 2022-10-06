81ef17b66e1e4661892462a17e4d48e309b71950
Kevin Templeton <templeton.kevin@gmail.com>
2022-10-05 19:27:08 -0500

    create UserEditForm

--- forms.py
+++ forms.py
@@ -23,3 +23,14 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class UserEditForm(FlaskForm):
+    """Form for updating users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = StringField('Bio')
+    password = PasswordField('Confirm Password', validators=[Length(min=6)])
